10c6464e495dc0fdc8690573e8aae5d22474dc4a
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-21 15:31:21 +0300

    Do 3.12

---
+++ mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const dbName = 'puhelinluettelo'
+
+const url =
+`mongodb+srv://arttu_fullstack:\
+${password}@mangoklusteri-0fsgj.\
+mongodb.net/${dbName}?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+switch (process.argv.length) {
+    case 3:
+        Person.find({})
+        .then(result => {
+            result.forEach(person => {
+                console.log(person)
+            })
+            mongoose.connection.close()
+        })
+        break;
+    case 5:
+        const newName = process.argv[3]
+        const newNum = process.argv[4]
+        const newPerson = new Person({
+            name: newName,
+            number: newNum,
+        })
+        newPerson.save()
+            .then(result => {
+                console.log(`added ${newName} number ${newNum} to phonebook`)
+                mongoose.connection.close()
+            })
+        break;
+    default:
+        console.log('example: "node mongo.js yourpassword Anna 040-1234556"')
+}
