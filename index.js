ae836584f1b7500ca3c65f3974c52b72eeccfa4a
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-21 12:14:49 +0300

    Do 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 
 const app = express()
@@ -14,6 +15,8 @@ morgan.token('bodydata', (request, response) => {
 
 app.use(morgan(':method :url :status '
     + ':res[content-length] - :response-time ms :bodydata'))
+
+app.use(cors())
 
 let persons = [
     {
